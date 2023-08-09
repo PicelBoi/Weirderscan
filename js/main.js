68b6e475985beb2438c7d179baf8ac580931d54b
Etan Zheng (PicelBoi) <etan.zheng09@gmail.com>
2023-08-09 17:18:06 -0400

    modified:   css/weatherscan.css  	renamed:    images/background.png -> images/5120x2880.png  	new file:   images/looksnicer.png  	modified:   index.html  	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 $(function(){
+	var indexbg = Math.floor(Math.random() * (20- 1)+ 1);
 	var $main = $("#main"),
 		$window = $( window ),
 	    mainHeight = $main.outerHeight(),
 	    mainWidth = $main.outerWidth(),
 	    mainAspect = 4/3,
 	    resizeTimer;
+	var $body = $("body");
+	document.getElementById("kirby").style.background = "url(https://raw.githubusercontent.com/PicelBoi/basically-qconrad-intellistar-but-worse/master/assets/background/" + indexbg + ".bg')";
 
 //calls rescale when window resizes
 	$(window).resize( function(e) {
